e8a2e208c65a5ba7fa0d2c1181bdb130f99ce6ee
ehchoi <chldlghk@gmail.com>
2022-07-07 17:37:51 +0900

    Change websocket endpoint.

--- src/main/java/chat/socket/config/WebSocketConfig.java
+++ src/main/java/chat/socket/config/WebSocketConfig.java
@@ -1,17 +1,27 @@
 package chat.socket.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+import org.springframework.web.socket.server.standard.ServerEndpointExporter;
 
 //websocket관련 설정 파일
 @Configuration  //컨테이너 등록
 @EnableWebSocketMessageBroker //웹소켓 서버 사 용 설정
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
+    /*
+    addEndpoint() : 클라이언트에서 websocket에 접속하기 위한 endpoint를 등록 (ex. localhost:8080/websocekt)
+     */
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/").setAllowedOrigins("*").withSockJS();
+        registry.addEndpoint("/websocket").setAllowedOrigins("*").withSockJS();
+    }
+
+    @Bean
+    public ServerEndpointExporter serverEndpointExporter() {
+        return new ServerEndpointExporter();
     }
 }
--- src/main/java/chat/socket/controller/MessageController.java
+++ src/main/java/chat/socket/controller/MessageController.java
@@ -1,7 +1,5 @@
 package chat.socket.controller;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
